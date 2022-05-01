e770b70be48036374ac98cf3ed85c0aef0c08ff7
RichardSmarsi <ricsmarsi@gmail.com>
2022-05-01 14:07:37 -0300

    Alterados os templates e rotas de login. Listando moedas por usuário na tela de profile

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
 from django.views.generic.base import TemplateView
-from .views import register, ContasView
+from .views import register, ContasView, ProfileView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')), #Adicionando as rotas do módulo de usuários padrão do DJango
     path('registrar/', register.as_view(), name='registrar'),
     path('', ContasView.as_view(), name='client_index'), #Chamando uma template view padrão.
-    path('usuario/', TemplateView.as_view(template_name='profile.html'), name='profile'),
+    path('perfil/', ProfileView.as_view(), name='profile'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CustomUsuarioCreateForm
 
+from core.models import Moeda_Usuario
+
 #from .models import Produto
 
 # Create your views here.
+
+class ContasView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        print(request.user.is_anonymous)
+
+        if request.user.is_anonymous: #Se o usuário não estiver logado
+            return render(request, 'contas.html')
+        else:
+            return redirect('profile')
+
+            
+
+'''
 class ContasView(TemplateView):
     template_name = 'contas.html'
 
@@ -15,6 +33,18 @@ class ContasView(TemplateView):
         
 
         #context['produtos'] = Produto.objects.all()
+
+        return context
+'''
+
+class ProfileView(TemplateView):
+    template_name = 'profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+
+        context['moedas'] = Moeda_Usuario.objects.filter(usuario=self.request.user)
+        print(context)
 
         return context
 
